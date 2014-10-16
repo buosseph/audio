5c793bcf65b99bd9a3791f2c44304054bfb9700f
Brian Uosseph <brian.uosseph@gmail.com>
2014-10-16 01:41:31 -0400

    Messing with file reading.

--- src/wave.rs
+++ src/wave.rs
@@ -3,74 +3,79 @@
 use audio::{RawAudio, SampleOrder};
 
 use std::str;
-use std::io::File;
+use std::io::{File, IoResult};
 use std::path::posix::{Path};
-
 
 // Sample = singular f64 value (independent of channel)
 // Clip = Group of samples along time domain (Should always include all channels)
 // Separate channels into separate tracks for processing
 
+
+const RIFF: u32 = 0x52494646;
+
+
 // All functions need to be rewritten!
 
 pub fn read_file_data(wav_file_path: &str) {
 	let path = Path::new(wav_file_path);
-	match File::open(&path) {
-		Ok(mut wav_file) => {
+	let mut wav_file = match File::open(&path) {
+		Ok(file)	=> file,
+		Err(e)		=> fail!("File error: {}", e),
+	};
 
-			// Assume 44 byte header for now
-			let riff_header = str::from_utf8_owned(wav_file.read_exact(4).unwrap()).unwrap();
-			let file_size = wav_file.read_le_u32().unwrap();
-			let file_type_header = str::from_utf8_owned(wav_file.read_exact(4).unwrap()).unwrap();
+	// Assume 44 byte header for now
+	let double_word = match wav_file.read_exact(4) {
+		Ok(bytes)	=> bytes,
+		Err(e)		=> fail!("Error: {}", e),
+	};
+	let riff_header = str::from_utf8(double_word.as_slice()).unwrap(); //str::from_utf8_owned(double_word).unwrap();
+	let file_size = wav_file.read_le_u32().unwrap();
+	let file_type_header = str::from_utf8_owned(wav_file.read_exact(4).unwrap()).unwrap();
 
-			let format_chunk_marker = str::from_utf8_owned(wav_file.read_exact(4).unwrap()).unwrap();
-			let format_chunk_length = wav_file.read_le_u32().unwrap();
-			let format_tag = wav_file.read_le_u16().unwrap();
+	let format_chunk_marker = str::from_utf8_owned(wav_file.read_exact(4).unwrap()).unwrap();
+	let format_chunk_length = wav_file.read_le_u32().unwrap();
+	let format_tag = wav_file.read_le_u16().unwrap();
 
-			let num_of_channels = wav_file.read_le_u16().unwrap();
-			let samples_per_sec = wav_file.read_le_u32().unwrap();
-			let data_rate = wav_file.read_le_u32().unwrap();
-			let block_size = wav_file.read_le_u16().unwrap();
-			let bit_rate = wav_file.read_le_u16().unwrap();
+	let num_of_channels = wav_file.read_le_u16().unwrap();
+	let samples_per_sec = wav_file.read_le_u32().unwrap();
+	let data_rate = wav_file.read_le_u32().unwrap();
+	let block_size = wav_file.read_le_u16().unwrap();
+	let bit_rate = wav_file.read_le_u16().unwrap();
 
-			let data_chunk_header = str::from_utf8_owned(wav_file.read_exact(4).unwrap()).unwrap();
-			let data_size = wav_file.read_le_u32().unwrap(); // Read this many bytes for data
+	let data_chunk_header = str::from_utf8_owned(wav_file.read_exact(4).unwrap()).unwrap();
+	let data_size = wav_file.read_le_u32().unwrap(); // Read this many bytes for data
 
 
-			println!(
-				"master_riff_chunk:
-					{}
-					File size: {}
-					File type: {}
-				{}_chunk:
-					Chunk length: {},
-					Format: {} (1 = PCM, 3 = IEEE float, ...),
-					Channels: {},
-					Sample rate: {},
-					Data rate: {},
-					Block size: {},
-					Bit rate: {}
-				{}_chunk:
-					Data size: {} bytes
-				",
-				riff_header,
-				file_size,
-				file_type_header,
-				format_chunk_marker,
-				format_chunk_length,
-				format_tag,
-				num_of_channels,
-				samples_per_sec,
-				data_rate,
-				block_size,
-				bit_rate,
-				data_chunk_header,
-				data_size
-				);
-
-		}
-		Err(e) => fail!("{}", e)
-	}
+	println!(
+		"master_riff_chunk:
+			{}
+			File size: {}
+			File type: {}
+		{}_chunk:
+			Chunk length: {},
+			Format: {} (1 = PCM, 3 = IEEE float, ...),
+			Channels: {},
+			Sample rate: {},
+			Data rate: {},
+			Block size: {},
+			Bit rate: {}
+		{}_chunk:
+			Data size: {} bytes
+		",
+		riff_header,
+		file_size,
+		file_type_header,
+		format_chunk_marker,
+		format_chunk_length,
+		format_tag,
+		num_of_channels,
+		samples_per_sec,
+		data_rate,
+		block_size,
+		bit_rate,
+		data_chunk_header,
+		data_size
+		);
 
 }
 
