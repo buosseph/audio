7d3824c989451184842826cbc017955c7a763e5c
Brian Uosseph <brian.uosseph@gmail.com>
2014-12-29 16:09:17 -0500

    Moved filter to separate module

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,11 @@
 
 // Audio structs
 pub use audio::RawAudio as RawAudio;
+pub use audio::SampleOrder as SampleOrder;
 
 // Traits
 pub use audio::Dynamics as Dynamics;
-// pub use audio::Utilities as Utilities;
+pub use audio::Utilities as Utilities;
 pub use audio::Filter as Filter;
 
 pub mod audio;
@@ -19,6 +20,7 @@ pub mod aiff;
 // Processing (Need to be refactored)
 // pub mod utilities;
 pub mod dynamics;
+pub mod filter;
 
 #[test]
 fn it_works() {
