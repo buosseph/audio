3c32a769161ed31911213018124c9e640696c705
Brian Uosseph <brian.uosseph@gmail.com>
2015-01-08 17:49:54 -0500

    Implemented frame header decoding

--- src/mp3/decoder.rs
+++ src/mp3/decoder.rs
@@ -6,22 +6,32 @@
  * - http://www.cs.columbia.edu/~sedwards/classes/2010/4840/reports/KH.pdf (Explaination and C implementation of MPEG-1 Layer III files)
  */
 
-use audio::RawAudio;
-use audio::SampleOrder;
-use std::io::{File, IoResult};
-use std::path::posix::Path;
+// Approach:
+// Start with MPEG-1 Layer III then MPEG-2 Layer III
+// Lower layers can come later, as those to are the most common
 
+use audio::{
+	AudioResult,
+	AudioError,
+	RawAudio,
+	SampleOrder
+};
+use std::io::{File};
 use super::{
 	BIT_RATE_HUFF_INDEX,
 	SAMP_FREQ_INDEX,
-	MODE_INDEX
+	MODE_INDEX,
+	MODE3_INDEX,
+	EXT_INDEX,
+	EXT3_INDEX,
+	EMPH_INDEX
 };
 
 // Frame and dependant structs
 #[derive(Show)]
 struct FrameHeader {			// Always 32 bits
-	syncword: int,				// 12 bits 	-> always 0xfff
-	id: int,					// 1 bit 	-> 1 = MPEG audio, 0 = reserved
+	syncword: int,				// 12 bits 	-> always 0xfff (= 4095)
+	version: int,				// 1 bit 	-> 1 = MPEG audio, 0 = reserved
 	layer: int,					// 2 bits 	-> 0x0 = reservec, 0x1 = Layer III, 0x2 = Layer II, 0x3 = Layer I (kinda silly)
 	protection_bit: int,		// 1 bit 	-> 1 = no added redundancy, 0 - redundancy added to bitstream
 	bit_rate_index: int,		// 4 bits 	-> decode using huffman, see specification for table
@@ -64,23 +74,52 @@ impl Frame {
 		unimplemented!();
 	}
 
-	fn read_header(slice: &[u8]) -> FrameHeader {
-		println!("{}", slice);
-		FrameHeader {
-			syncword: 0,
-			id: 0,
-			layer: 0,
-			protection_bit: 0,
-			bit_rate_index: 0,
-			sampling_frequency: 0,
-			padding_bit: false,
-			private_bit: 0,
-			mode: "Mono".to_string(),
-			mode_extension: "None".to_string(),
-			copyright: false,
-			original: false,
-			emphasis: "None".to_string(),
+	fn read_header(slice: &[u8]) -> AudioResult<FrameHeader> {
+		// println!("{}", slice);
+
+		let mp3_syncword = ( (slice[0] as u16) << 8 | (slice[1] as u16) ) >> 4;
+		if mp3_syncword != 4095 as u16 {
+			return Err(AudioError::FormatError("Not a valid MP3 frame".to_string()))
 		}
+
+		let version_id = slice[1] << 4 >> 7;
+		let layer_id = slice[1] << 5 >> 6;
+		let protection_bit = slice[1] << 7 >> 7;
+
+		let kbps = BIT_RATE_HUFF_INDEX[(slice[2] >> 4) as uint];
+		let frequency = SAMP_FREQ_INDEX[(slice[2] << 4 >> 7) as uint];
+		let pad_bit: bool = match slice[2] << 6 >> 7 { 0 => false, _ => true};
+		let priv_bit = slice[2] << 7 >> 7;
+
+		// Probably put layer_id as enum to make more sense!
+		let mode = match layer_id as int {
+			1 => MODE3_INDEX[(slice[3] >> 6) as uint],
+			_ => MODE_INDEX[(slice[3] >> 6) as uint]
+		};
+		let extension = match layer_id as int {
+			1 => EXT3_INDEX[(slice[3] << 2 >> 6) as uint],
+			_ => EXT_INDEX[(slice[3] << 2 >> 6) as uint]
+		};
+
+		let copy_bit: bool = match slice[3] << 4 >> 7 { 0 => false, _ => true};
+		let home_bit: bool = match slice[3] << 5 >> 7 { 0 => false, _ => true};
+		let emphasis = EMPH_INDEX[(slice[3] << 6 >> 7) as uint];
+
+		Ok(FrameHeader {
+			syncword: mp3_syncword as int,
+			version: version_id as int,
+			layer: layer_id as int,
+			protection_bit: protection_bit as int,
+			bit_rate_index: kbps as int,
+			sampling_frequency: frequency as int,
+			padding_bit: pad_bit,
+			private_bit: priv_bit as int,
+			mode: mode.to_string(),
+			mode_extension: extension.to_string(),
+			copyright: copy_bit,
+			original: home_bit,
+			emphasis: emphasis.to_string(),
+		})
 	}
 
 	fn error_check(&mut self) {
@@ -91,7 +130,7 @@ impl Frame {
 	}
 }
 
-
+// Maybe unnecessary
 #[derive(Show)]
 struct Bitstream {
 	// stream: String,
@@ -106,7 +145,7 @@ impl Bitstream {
 
 
 
-pub fn read_file_meta(file_path: &str) -> IoResult<()> {
+pub fn read_file_meta(file_path: &str) -> AudioResult<()> {
 	println!("{}", file_path);
 	let path = Path::new(file_path);
 	let mut file = try!(File::open(&path));
--- src/mp3/mod.rs
+++ src/mp3/mod.rs
@@ -4,3 +4,9 @@ pub mod decoder;
 const BIT_RATE_HUFF_INDEX: [u16; 16] = [ 0, 32, 40, 48, 56, 64, 80, 96, 112, 128, 160, 192, 224, 256, 320, 0 ];
 const SAMP_FREQ_INDEX: [f64; 4] = [ 44100f64, 48000f64, 32000f64, -1f64 ];
 const MODE_INDEX: [&'static str; 4] = [ "stereo", "joint_stereo", "dual_channel", "single_channel" ];
+const MODE3_INDEX: [&'static str; 4] = [ "stereo", "intensity stereo and/or ms stereo", "dual_channel", "single_channel" ];
+
+// In a rush fix up later
+const EXT_INDEX: [&'static str; 4] = ["bound==4","bound==8","bound==12","bound==16"];
+const EXT3_INDEX: [&'static str; 4] = ["i: off, ms: off","i: on, ms: off","i: off, ms: on","i: on, ms: on"];
+const EMPH_INDEX: [&'static str; 4] = ["None","50/15 ms","reserved","CCITT J.17"];
