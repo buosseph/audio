e3ec173ca763dc80b86fb53d74a3753f80402bae
Brian Uosseph <brian.uosseph@gmail.com>
2015-01-09 14:18:41 -0500

    Started implementing SideInfo, and trying to reimplement Frame to obtain data bytes since they are not contained in the physical frame

--- src/mp3/decoder.rs
+++ src/mp3/decoder.rs
@@ -2,7 +2,8 @@
  * -- For frame decoding --
  * - http://mpgedit.org/mpgedit/mpeg_format/mpeghdr.htm
  * 
- *
+ * -- For side_info decoding --
+ * - http://sea-mist.se/fou/cuppsats.nsf/all/857e49b9bfa2d753c125722700157b97/$file/Thesis%20report-%20MP3%20Decoder.pdf
  *
  * - http://cutebugs.net/files/mpeg-drafts/11172-3.pdf (The specification)
  * - http://blog.bjrn.se/2008/10/lets-build-mp3-decoder.html (Some explaination for a Haskell implementation)
@@ -22,7 +23,7 @@ use audio::{
 };
 use std::io::{File};
 use super::{
-	BIT_RATE_HUFF_INDEX,
+	KBPS_INDEX,
 	SAMP_FREQ_INDEX,
 	MODE_INDEX,
 	MODE3_INDEX,
@@ -39,22 +40,27 @@ enum Layer {
 	Reserved 	= 0x0
 }
 
-// Frame and dependant structs
 #[derive(Show)]
 struct FrameHeader {			// Always 32 bits
 	syncword: int,				// 12 bits 	-> always 0xfff (= 4095)
 	version: int,				// 1 bit 	-> 1 = MPEG audio, 0 = reserved (or MPEG2?)
 	layer: Layer,				// 2 bits 	-> 0x0 = reservec, 0x1 = Layer III, 0x2 = Layer II, 0x3 = Layer I (kinda silly)
-	protection_bit: int,		// 1 bit 	-> 1 = no added redundancy, 0 - redundancy added to bitstream (CRC)
+	has_crc: bool,				// 1 bit 	-> 1 = no added redundancy, 0 - redundancy added to bitstream (CRC)
 	kbps: int,					// 4 bits 	-> decode using huffman, see specification for table
 	sample_rate: int,			// 2 bits 	-> 0x0 = 44.1kHz, 0x1 = 48kHz, 0x10 = 32kHz, 0x11 = reserved
-	padding: bool,			// 1 bit 	-> see spec, kinda hard to put into comment
+	padding: bool,				// 1 bit 	-> padding determined by layer
 	private_bit: int,			// 1 bit 	-> not used in future
 	mode: String,				// 2 bits 	-> 0x0 = Stereo, 0x1 = Joint Stereo, 0x2 = Dual Channel, 0x3 = Single Channel
 	mode_extension: String,		// 2 bits 	-> Used if mode = Joint Stereo, see spec
 	copyright: bool,			// 1 bit 	-> 0x1 = copyright protected
 	original: bool,				// 1 bit 	-> 0x1 = is original, for lib purposes this will probably always be 0 (copy)
 	emphasis: String,			// 2 bits 	-> type of de-emphasis used, see spec
+}
+
+#[derive(Show)]
+struct SideInfo {
+	main_data_begin: int,	// location of start of main data relative from frame header, if 0 then starts after side info, else it's n bytes prior to current frame header in bitstream
+	share: int,				// ???
 }
 
 // Every frame has n slots. In Layer I, one slot is 4 bytes long.
@@ -74,10 +80,11 @@ struct FrameHeader {			// Always 32 bits
 // 
 #[derive(Show)]
 struct Frame<'f> {
-	length: uint,
+	physical_length: uint,
+	original_header: &'f [u8],	// for debugging
 	header: FrameHeader,
-	// side_info: SideInfo,
-	data: &'f [u8],
+	side_info: SideInfo,
+	// data: &'f [u8],	// audio data for frame isn't contained in 'physical frame', thus can be in two frames
 }
 impl<'f> Frame<'f> {
 	/// Decodes frame header information from first four bytes of frame, which are provided as a slice
@@ -88,18 +95,24 @@ impl<'f> Frame<'f> {
 		}
 
 		let version_id = slice[1] << 4 >> 7;
+		if version_id != 1 {
+			return Err(AudioError::UnsupportedError("This is not MPEG-1 audio".to_string()))
+		}
 		let layer: Layer =
 			match slice[1] << 5 >> 6 {
-				1 => Layer::LayerIII,
-				2 => Layer::LayerII,
-				3 => Layer::LayerI,
+				1 => Layer::LayerIII,	// mp3
+				2 => Layer::LayerII,	// mp2
+				3 => Layer::LayerI,		// mp1
 				_ => Layer::Reserved 
 			};
-		let protection_bit = slice[1] << 7 >> 7;
+		if layer != Layer::LayerIII {
+			return Err(AudioError::UnsupportedError("This is not Layer III audio (not MP3)".to_string()))
+		}
+		let protection_bit: bool = match slice[1] << 7 >> 7 { 0 => true, _ => false };
 
-		let kbps = BIT_RATE_HUFF_INDEX[(slice[2] >> 4) as uint];
+		let kbps = KBPS_INDEX[(slice[2] >> 4) as uint];
 		let frequency = SAMP_FREQ_INDEX[(slice[2] << 4 >> 7) as uint];
-		let pad_bit: bool = match slice[2] << 6 >> 7 { 0 => false, _ => true};
+		let pad_bit: bool = match slice[2] << 6 >> 7 { 0 => false, _ => true };
 		let priv_bit = slice[2] << 7 >> 7;
 
 		let mode = match layer {
@@ -119,7 +132,7 @@ impl<'f> Frame<'f> {
 			syncword: mp3_syncword as int,
 			version: version_id as int,
 			layer: layer,
-			protection_bit: protection_bit as int,
+			has_crc: protection_bit,
 			kbps: kbps as int,
 			sample_rate: frequency as int,
 			padding: pad_bit,
@@ -131,8 +144,40 @@ impl<'f> Frame<'f> {
 			emphasis: emphasis.to_string(),
 		})
 	}
+
+	fn read_side_info(slice: &[u8]) -> AudioResult<SideInfo> {
+		match slice.len() {
+			32 => Frame::read_dual_side_info(slice),
+			17 => Frame::read_single_side_info(slice),
+			_  => return Err(AudioError::FormatError(
+				"Incorrect slice length provided for decoding frame side info".to_string()))
+		}
+	}
+
+	fn read_single_side_info(slice: &[u8]) -> AudioResult<SideInfo> {
+		let start_ptr = (((slice[0] as u16) << 8 | ((slice[1] >> 7 << 7) as u16)) >> 7) as int;	// remember this is a negative offset
+		// let priv_bits = (slice[1] << 1 >> 3);	// 5 bits
+		let share = ((slice[1] << 6 >> 4) | (slice[2] >> 6)) as int;
+		Ok(SideInfo{
+			main_data_begin: start_ptr,
+			share: share,
+		})
+	}
+
+	fn read_dual_side_info(slice: &[u8]) -> AudioResult<SideInfo> {
+		let start_ptr = (((slice[0] as u16) << 8 | ((slice[1] >> 7 << 7) as u16)) >> 7) as int;	// remember this is a negative offset
+		// let priv_bits = (slice[1] << 1 >> 5);	// 3 bits
+		// println!("{}", start_ptr);
+		Ok(SideInfo{
+			main_data_begin: start_ptr,
+			share: 0
+		})
+	}
 }
 
+// Need seek_frame to find first valid frame (MP3s usually have a preceeding ID3 tag).
+// Since there can be a lot of data that can contain false syncwords, will need to decode
+// the candidate header and the next frame header for validation.
 #[derive(Show)]
 struct Bitstream {
 	// stream: String,
@@ -142,16 +187,21 @@ impl Bitstream {
 	fn read_frames(&mut self) -> AudioResult<Vec<Frame>> {
 		let mut v: Vec<Frame> = Vec::new();
 		let mut i = 0;
+		let mut data_ptr: uint = 0;
 		while i < self.buffer.len() {
-			let frame = try!(self.read_frame(i));
-			i += frame.length;
+			let frame = try!(self.read_frame(i, /*data_ptr*/));
+			i += frame.physical_length;
 			v.push(frame);
 		}
+		println!("{}\n{}", v[2], v[2].side_info.main_data_begin);
 		Ok(v)
 	}
 
-	fn read_frame(&self, buffer_pos: uint) -> AudioResult<Frame> {
-		let header: FrameHeader = try!(Frame::read_header(self.buffer.slice(buffer_pos, buffer_pos + 4)));
+	fn read_frame(&self, header_pos: uint, /*prev_data_ptr: &mut uint*/) -> AudioResult<Frame> {
+		let mut start_pos: uint = header_pos;
+		let mut end_pos: uint = header_pos + 4;
+		let orig = self.buffer.slice(start_pos, end_pos); // Only for debugging
+		let header: FrameHeader = try!(Frame::read_header(self.buffer.slice(start_pos, end_pos)));
 		let padding_length: int = // in bytes (is it suppose to be bits? Doubt it)
 			match (&header.layer, header.padding){
 				(&Layer::LayerI, true) 	=> 4,
@@ -165,13 +215,33 @@ impl Bitstream {
 				_ => (144 * (header.kbps * 1000) / header.sample_rate + padding_length) as uint
 			};
 
-		let data_length : uint 	= frame_length - 4; // frame_length includes header bytes
-		let buffer 		: &[u8] = self.buffer.slice(buffer_pos + 4, buffer_pos + 4 + data_length);
+		// Skip CRC for now
+		let crc_length: uint = match header.has_crc { true => 2, false => 0 };
+		start_pos = end_pos;
+		end_pos += crc_length;
+
+		let side_info_length: uint =	// I think this is the same between all layers. Not important right now
+			match header.mode.as_slice() {
+				"single_channel" 	=> 17,
+				_					=> 32
+			};
+		start_pos 				= end_pos;
+		end_pos 				+= side_info_length;
+		let side_info: SideInfo = try!(Frame::read_side_info(self.buffer.slice(start_pos, end_pos)));
+
+		// let data_length : uint 	= frame_length - 4 - side_info_length;
+		// start_pos				= end_pos;
+		// end_pos 				+= data_length;
+		// let buffer 		: &[u8] = self.buffer.slice(start_pos, end_pos);
+
+		// let data_start: uint = header_pos - side_info.main_data_begin;	// This still includes frame headers and side info
 
 		Ok(Frame{
-			length: frame_length,
+			physical_length: frame_length,
+			original_header: orig,	// for debugging
 			header: header,
-			data: 	buffer
+			side_info: side_info,
+			// data: 	buffer
 		})
 	}
 }
