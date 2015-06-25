e07fbe086b4ee5b42846abd94155bb8566cd9a2b
Brian Uosseph <brian.uosseph@gmail.com>
2015-06-25 00:11:50 -0400

    Added more benches

--- benches/lib.rs
+++ benches/lib.rs
@@ -6,10 +6,28 @@ use std::path::Path;
 use test::Bencher;
 
 #[bench]
+fn empty(b: &mut Bencher) {
+  b.iter(|| 1);
+}
+
+#[bench]
 fn read_wave_track(b: &mut Bencher) {
   b.iter(|| {
     audio::open(&Path::new("tests/wav/Warrior Concerto - no meta.wav"))
       .ok().expect("Couldn't read file");
+  });
+}
+
+#[bench]
+fn write_wave_track(b: &mut Bencher) {
+  let audio = audio::open(
+    &Path::new("tests/wav/Warrior Concerto - no meta.wav")
+  ).ok().expect("Couldn't read file");
+  b.iter(|| {
+    audio::save(
+      &Path::new("tests/results/tmp_i16.wav"),
+      &audio
+    ).ok().expect("Couldn't write file");
   });
 }
 
@@ -21,16 +39,33 @@ fn read_aiff_track(b: &mut Bencher) {
   });
 }
 
+#[bench]
+fn write_aiff_track(b: &mut Bencher) {
+  let audio = audio::open(
+    &Path::new("tests/aiff/Warrior Concerto - no meta.aiff")
+  ).ok().expect("Couldn't read file");
+  b.iter(|| {
+    audio::save(
+      &Path::new("tests/results/tmp_i16.aiff"),
+      &audio
+    ).ok().expect("Couldn't write file");
+  });
+}
+
+
 /*
  *  The benchmarks below are just to have some comparision
  *  with strictly reading from a File or BufReader.
  *
  *  One set of bench results for a quick reference
  *
- *  test buf_reader      ... bench: 102,721,113 ns/iter (+/- 23,721,150)
- *  test file            ... bench:  73,460,776 ns/iter (+/- 36,662,689)
- *  test read_aiff_track ... bench: 279,125,245 ns/iter (+/- 35,967,603)
- *  test read_wave_track ... bench: 326,092,886 ns/iter (+/- 69,465,665)
+ *  test buf_reader       ... bench: 104,821,811 ns/iter (+/- 27,349,621)
+ *  test empty            ... bench:           0 ns/iter (+/- 0)
+ *  test file             ... bench:  64,956,282 ns/iter (+/- 25,325,489)
+ *  test read_aiff_track  ... bench: 327,196,191 ns/iter (+/- 83,703,581)
+ *  test read_wave_track  ... bench: 356,682,279 ns/iter (+/- 55,742,915)
+ *  test write_aiff_track ... bench: 843,659,323 ns/iter (+/- 519,412,469)
+ *  test write_wave_track ... bench: 709,889,187 ns/iter (+/- 157,425,662)
  *
  */
 
